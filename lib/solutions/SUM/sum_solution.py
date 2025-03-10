61eb438286103f403a9df80b7a8c01baef27838d
sbx_user1051 <sbx_user1051@169.254.9.47>
2025-03-10 19:45:13 +0000

    Mon Mar 10 19:45:13 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,14 +1,9 @@
-# noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     def sum(a, b):
-    """
-    Sums two positive integers between 0 and 100.
+        if 0 <= a <= 100 and 0 <= b <= 100:
+            return a + b
+        else:
+            raise ValueError("Both parameters must be between 0 and 100")
+    
+    return sum(x, y)
 
-    :param a: a positive integer between 0-100
-    :param b: a positive integer between 0-100
-    :return: an integer representing the sum of the two numbers
-    """
-    if 0 <= a <= 100 and 0 <= b <= 100:
-        return a + b
-    else:
-        raise ValueError("Both parameters must be between 0 and 100")
